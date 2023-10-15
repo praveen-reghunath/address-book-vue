e78db620cdcbdc88ff261e947425d1cc46dfda21
Praveen <6327769+praveen-reghunath@users.noreply.github.com>
2023-10-14 19:31:38 -0700

    create view

--- src/views/DisplayView.vue
+++ src/views/DisplayView.vue
@@ -3,7 +3,31 @@
     <div class="actions">
       <RouterLink class="edit" :to="`/input/${contactId}`">Edit</RouterLink>
     </div>
-    <div class=""></div>
+    <div class="view">
+      <div class="row">
+        <label for="" class="label">Given Name</label>
+        <div>John</div>
+      </div>
+      <div class="row">
+        <label for="" class="label">Surname</label>
+        <div>Smith</div>
+      </div>
+      <div class="row">
+        <label for="" class="label">Home Phone</label>
+        <div>651-410-8490</div>
+      </div>
+      <div class="row">
+        <label for="" class="label">Home Phone</label>
+        <div>651-410-8490</div>
+      </div>
+      <div class="row">
+        <label for="" class="label">Home Address</label>
+        <div>
+          <div>38680 Hastings st, Apt 221</div>
+          <div>Fremont, CA 94536</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -34,5 +58,14 @@ onBeforeRouteUpdate(async (to, from) => {
     color: blue;
   }
 
+  .row {
+    display: flex;
+    padding: 0 10%;
+    margin-top: 2rem;
+  }
+
+  .label {
+    width: 40%;
+  }
 }
 </style>
